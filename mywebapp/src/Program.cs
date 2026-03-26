fc67b231a5e1981967b90ecb3dac938fd9ec172b
Eeshah Qamar <eeshah.qamar24@gmail.com>
2026-03-27 02:49:15 +0500

    Fix CS5001: Implement .NET 6 Top-Level Statements

--- mywebapp/src/Program.cs
+++ mywebapp/src/Program.cs
@@ -1,10 +1,6 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "CI/CD Demo Success!");
+app.MapGet("/", () => "Build Success!");
 
 app.Run();
