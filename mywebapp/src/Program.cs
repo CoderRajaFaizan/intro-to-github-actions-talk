5f44a230083f5db0b25c68952562688e16769520
Eeshah Qamar <eeshah.qamar24@gmail.com>
2026-03-27 04:19:13 +0500

    Fix: Add Program.cs entry point and align artifact paths

--- mywebapp/src/Program.cs
+++ mywebapp/src/Program.cs
@@ -1,9 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Final CI/CD Success!");
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
 
 
